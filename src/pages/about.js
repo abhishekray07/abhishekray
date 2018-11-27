88f9e15bbf3cb3df9ddde1ada6e3f8fb081380d4
Abhishek Ray <abhishek.ray@robinhood.com>
2018-11-27 12:41:51 -0800

    Play around with stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header-test"
 
 export default () => (
     <div style={{ color: `teal`}}>
-        <Header />
-        <h1>About Gatsby</h1>
+        <Link to="/contact">Contact</Link>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's pretty cool!" />
         <p>Such wow. Very react.</p>
     </div>
 )
