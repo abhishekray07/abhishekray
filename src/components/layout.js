135638488df9c7b965acbe2a9cbc764b53cd9fa5
Abhishek Ray <abhishekray07@gmail.com>
2019-05-29 21:09:36 -0700

    Formatting changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,15 +21,15 @@ export default ({ children }) => (
     }
     render={data => (
       <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-        <header style={{ marginBottom: `1.5rem` }}>
-          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <header style={{ marginTop: `1.5rem`, marginBottom: `1.5rem` }}>
+          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, textDecoration: `none` }}>
             <h3 style={{ display: `inline` }}>
               {data.site.siteMetadata.title}
             </h3>
           </Link>
           <ul style={{ listStyle: `none`, float: `right` }}>
+            <ListLink to="/books/">Books</ListLink>
             <ListLink to="/about/">About</ListLink>
-            <ListLink to="/contact/">Contact</ListLink>
           </ul>
         </header>
         {children}
