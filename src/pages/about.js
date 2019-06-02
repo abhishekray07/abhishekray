1b8d2629c65a39ce45a8a9faa088b0a9d78d31f9
Abhishek Ray <abhishekray07@gmail.com>
2019-06-02 15:42:25 -0700

    Replace blog generator with Hugo

--- src/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default () => (
-  <div>
-    <h1>About me</h1>
-    <p>Please, fill this up!</p>
-  </div>
-)
--- src/pages/welcome.md
+++
@@ -1,6 +0,0 @@
----
-title: "Welcome!"
-date: "2018-12-03"
----
-
-Thank you for visiting my blog!
